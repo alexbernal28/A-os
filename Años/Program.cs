b5f64871892b783219b06a8558c6f1ea17c50d28
alexa <alexa@DESKTOP-QEHHSV3>
2021-11-29 19:49:59 -0400

    Agregar archivos de proyecto.

---
+++ Años/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Años
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Ingresa tu fecha de nacimiento:");
+                int fechaNac = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ingresa una fecha futura");
+                int fechaFu = Convert.ToInt32(Console.ReadLine());
+
+                const int hoy = 2021;
+
+                int edad = hoy - fechaNac;
+
+                int futuro = fechaFu - fechaNac;
+
+                if (fechaNac <= hoy)
+                {
+                    Console.WriteLine("Tu edad es: " + edad + " años.");
+                }
+                else
+                {
+                    Console.WriteLine("Todavia no haz nacido");
+                }
+
+                if (fechaNac <= fechaFu)
+                {
+                    Console.WriteLine("En este año tendras: " + futuro + " años.");
+                }
+                else
+                {
+                    Console.WriteLine("Esta fecha ya paso");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error, Valor incorrecto");
+                
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
